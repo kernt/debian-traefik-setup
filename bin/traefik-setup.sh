dc59a040b47b0b2e4571d2034a6dbddd457c66f9
Tobias Kern <tobkern1980@gmail.com>
2025-10-29 16:04:32 +0100

    new docker and traefik version coustomasations

--- bin/traefik-setup.sh
+++ bin/traefik-setup.sh
@@ -28,23 +28,14 @@ Setup_Docker() {
 
     sudo apt update
 
-    sudo apt -y install docker-ce docker-ce-cli containerd.io
-}
-
-Setup_Docker_Compose() {
-    printf "\n---> INSTALL DOCKER COMPOSE <---\n"
-    local _download_url="https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)"
-    sudo curl -L "${_download_url}" -o /usr/local/bin/docker-compose
-    sudo chmod ug+x /usr/local/bin/docker-compose
+    sudo apt -y install docker-ce docker-ce-cli containerd.io docker-ce-rootless-extras docker-compose-plugin
 }
 
 Setup_Traefik() {
     printf "\n---> INSTALL TRAEFIK <---\n"
 
     sudo cp -r ./traefik /opt
-
     cd /opt/traefik
-
     sudo touch acme.json
 
     # Traefik will not create the certificates if we don't fix the permissions
@@ -74,13 +65,13 @@ Setup_Traefik() {
 
     # Traefik will be listening on port 80 and 443, and proxy the requests to
     #  the associated container for the domain. Check the README for more details.
-    sudo docker-compose up -d traefik
+    sudo docker compose up -d traefik
 
     # Just give sometime for it to start in order to check the logs afterwards.
     sleep 5
 
     printf "\n---> CHECK TRAEFIK LOGS <---\n"
-    sudo docker-compose logs traefik
+    sudo docker compose logs traefik
 
     cd -
 }
@@ -88,14 +79,13 @@ Setup_Traefik() {
 Main() {
     Setup_Depedencies
     Setup_Docker
-    Setup_Docker_Compose
     Setup_Traefik
 
     printf "\n\n---> DOCKER VERSION <---\n"
     sudo docker version
 
     printf "\n---> DOCKER COMPOSE VERSION <---\n"
-    sudo docker-compose --version
+    sudo docker compose --version
     echo
 
     printf "\n---> GIT VERSION<---\n"
@@ -108,16 +98,16 @@ Main() {
     printf "\nSome useful examples:\n"
 
     printf "\n## Start Traefik:\n"
-    printf "sudo docker-compose up -d traefik\n"
+    printf "sudo docker compose up -d traefik\n"
 
     printf "\n## Restart Traefik:\n"
-    printf "sudo docker-compose restart traefik\n"
+    printf "sudo docker compose restart traefik\n"
 
     printf "\n## Destroy Traefik:\n"
-    printf "sudo docker-compose down\n"
+    printf "sudo docker compose down\n"
 
     printf "\n## Tailing the Traefik logs in realtime:\n"
-    printf "sudo docker-compose logs --follow traefik\n"
+    printf "sudo docker compose logs --follow traefik\n"
 
     printf "\n---> TRAEFIK is now listening for new docker containers <---\n\n"
 }
